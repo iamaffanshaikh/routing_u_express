1719eff50b45d290bb7a2e6a86f7de81c4a9ab32
iamaffanshaikh <iamaffanshaik71@gmail.com>
2024-02-19 01:13:21 +0530

    added error and few more things

--- script.js
+++ script.js
@@ -6,22 +6,39 @@ app.use((req, res, next) => {
     next();
 })
 
-app.get("/", function (req, res){
-    res.send("Server is Initilallised!");
+app.use(express.static("./public"))
+
+app.set("view engine", "ejs")
+
+
+app.get("/error", function(req, res, next){
+    throw error 
 })
 
-app.get("/contact", function(req, res){
-    res.send("This is Contact page!");
+app.get("/", function (req, res){
+    res.send("Server is Initilallised!");
 })
 
 app.get("/profile/:username", (req, res) =>{
     res.send(`Hello From ${req.params.username}`)
 })
 
-app.set("view engine", "ejs")
-
 app.get("/home", function(req, res){
     res.render("index", {Manejando: "Priority"})
 })
+
+app.get("/contact", function(req, res){
+    res.render("contact")
+})
+
+app.use(
+    function errorHandler (err, req, res, next) {
+        if (res.headersSent) {
+          return next(err)
+        }
+        res.status(500)
+        res.render('error', { error: "Found an error" })
+      }
+)
 
 app.listen(3000);
